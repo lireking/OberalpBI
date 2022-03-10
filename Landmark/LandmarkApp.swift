865a683445075fc257924827fc3cc0cee1895f43
Michael Bernhart <michael.bernhart@oberalp.com>
2022-03-10 16:46:48 +0100

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by OakHost Customer on 10/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
